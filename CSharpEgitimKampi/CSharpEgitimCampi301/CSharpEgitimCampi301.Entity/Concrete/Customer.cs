07887e49173da9e1214cf021d3a046a7ca445b6b
Seher ÇELİK <89248368+sehercelikk@users.noreply.github.com>
2025-06-20 14:06:22 +0300

    Add files via upload

---
+++ CSharpEgitimKampi/CSharpEgitimCampi301/CSharpEgitimCampi301.Entity/Concrete/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimCampi301.Entity.Concrete
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerSurname { get; set; }
+        public bool Status { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }
+        public List<Order> Orders { get; set; }
+    }
+}
